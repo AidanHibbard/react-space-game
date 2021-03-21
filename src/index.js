5c4321c5d1e8e8f3c0dd6dfc6fd1e53654102211
Aidan Hibbard <42854121+AidanHibbard@users.noreply.github.com>
2021-03-21 11:34:50 -0700

    Ship Movement, floor issues
    Created group around the ship primitive to add a reference to the ship that controls are bound to, the ship now tracks the mouse.
    
    Tried adding a texture image to the floor plane the problem is that I get a constructor error which I'm assuming comes from the addition of the texture variable within the floor component
    
    The floor has also been moved into the suspense component within app so that it "lazy loads"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import * as THREE from 'three'
 import SmallFighter from './models/SmallFighter.gltf'
-import React, { Suspense, useRef } from 'react';
+import background from './images/background.png'
+import React, { Suspense, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { 
@@ -11,6 +13,7 @@ import {
 } from 'react-three-fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { MeshBasicMaterial } from 'three';
 
 //Floor height
 const FloorHeight = -100;
@@ -37,13 +40,32 @@ function LoadingState() {
 }
 // Ship model
 function Fighter() {
-    const ship = useLoader(GLTFLoader, SmallFighter)
-    return <primitive object={ship.scene} position={[0, 0, 0]} />
+    const [shipPosition, setShipPosition] = useState()
+    const ship = useRef()
+    useFrame(({mouse}) => {
+        setShipPosition({
+            position: { x: mouse.x * 6, y: mouse.y * 2 },
+            rotation: { z: -mouse.x * 0.5, x: -mouse.x * 0.5, y: -mouse.y * 0.2 },
+        })
+    })
+    useFrame(() => {
+        ship.current.rotation.z = shipPosition.rotation.z;
+        ship.current.rotation.y = shipPosition.rotation.x;
+        ship.current.rotation.x = shipPosition.rotation.y;
+        ship.current.position.y = shipPosition.position.y;
+        ship.current.position.x = shipPosition.position.x;
+    })
+    const gltf = useLoader(GLTFLoader, SmallFighter)
+    return (
+        <group ref={ship}>
+            <primitive object={gltf.scene} position={[0, 0, 0]} />
+        </group>
+    )
 }
 // Ground layer (space)
-
 function Floor() {
     const floor = useRef()
+    const texture = useLoader(THREE.TextureLoader, background)
     return (
         <mesh 
             visible
@@ -52,13 +74,7 @@ function Floor() {
             ref={floor}
         >
             <planeBufferGeometry attach="geometry" args={[5000, 5000, 128, 128]} />
-            <meshStandardMaterial
-                attach="material"
-                color="white"
-                roughness={1}
-                metalness={0}
-                wireframe
-            />
+            <MeshBasicMaterial attach="material" map={texture}/>
         </mesh>
     )
 }
@@ -70,8 +86,9 @@ const CameraControls = () => {
         gl: { domElement },
     } = useThree();
     const controls = useRef()
-    // I have no idea if state is neccessary or not, Ive tested without it and the controls work but Im leaving it in
+    // I have no idea if (state) is neccessary or not, Ive tested without it and the controls work but Im leaving it in because it may be useful later
     useFrame((state) => controls.current.update())
+    // Add below to controls to
     // maxAzimuthAngle={Math.PI / 4} maxPolarAngle={Math.PI} minAzimuthAngle={-Math.PI / 4} minPolarAngle={0}
     return <orbitControls ref={controls} args={[camera, domElement]}/>;
 }
@@ -85,8 +102,8 @@ function App() {
             <directionalLight intensity={0.5} />
             <Suspense fallback={<LoadingState />}>
                 <Fighter />
+                <Floor />
             </Suspense>
-            <Floor />
         </Canvas>
     )
 }
